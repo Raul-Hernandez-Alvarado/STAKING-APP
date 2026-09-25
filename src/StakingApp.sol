303d676b5a6549e749a8749b7e720e4b5860cab8
Raúl Hernández Alvarado <raulhernandezalvarado16@gmail.com>
2026-09-25 18:46:04 +0200

    Gas optimitation

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -7,10 +7,17 @@ import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20
 
 contract StakingApp is Ownable {  
 
+    error InvalidStakingAmount();
+    error ActiveStakeExists();
+    error TransferFailed();
+    error NoActiveStake();
+    error StakingPeriodNotElapsed();
+    error RewardTransferFailed();
+
     address public stakingToken;
-    uint256 public stakingPeriod;
-    uint256 public fixedStakingAmount;
-    uint256 public rewardPerPeriod;
+    uint96 public fixedStakingAmount;
+    uint64 public stakingPeriod;
+    uint128 public rewardPerPeriod;
     mapping(address => uint256) public usersBalance;
     mapping(address => uint256) public depositTime;
 
@@ -20,51 +27,58 @@ contract StakingApp is Ownable {
     event ClaimRewards(address indexed user, uint256 rewardAmount);
     event EtherSend(uint256 amount);
 
-    constructor(address _tokenAddress, address _owner, uint256 _stakingPeriod, uint256 _fixedStakingAmount, uint256 _rewardPerPeriod) Ownable(_owner) { 
+    constructor (
+        address _tokenAddress, 
+        address _owner, 
+        uint64 _stakingPeriod, 
+        uint96 _fixedStakingAmount, 
+        uint128 _rewardPerPeriod
+    ) Ownable(_owner) {  
         stakingToken = _tokenAddress;
         stakingPeriod = _stakingPeriod;
         fixedStakingAmount = _fixedStakingAmount;
         rewardPerPeriod = _rewardPerPeriod;
     }
 
-    function setStakingPeriod(uint256 _stakingPeriod) external onlyOwner { 
+    function setStakingPeriod(uint64 _stakingPeriod) external onlyOwner { 
         stakingPeriod = _stakingPeriod;
-
         emit ChangeStakingPeriod(_stakingPeriod);
     }
 
-    function setFixedStakingAmount(uint256 _fixedStakingAmount) external onlyOwner { 
+    function setFixedStakingAmount(uint96 _fixedStakingAmount) external onlyOwner { 
         fixedStakingAmount = _fixedStakingAmount;
     }
 
-    function deposit(uint256 _amount) external { 
-        require(_amount == fixedStakingAmount, "Only the fixed staking amount can be staked at a time");
-        require(usersBalance[msg.sender] == 0, "You already have an active stake");
+    function deposit(uint96 _amount) external { 
+        if (_amount != fixedStakingAmount) revert InvalidStakingAmount();
+        if (usersBalance[msg.sender] != 0) revert ActiveStakeExists();
 
         bool success = IERC20(stakingToken).transferFrom(msg.sender, address(this), _amount);
-        require(success, "Failed to transfer staking token");
-        usersBalance[msg.sender] += _amount;
+        if (!success) revert TransferFailed();
+
+        usersBalance[msg.sender] = _amount;
         depositTime[msg.sender] = block.timestamp;
 
         emit Deposit(msg.sender, _amount);
     }
 
     function withdraw() external { 
-        require(usersBalance[msg.sender] > 0, "You don't have an active stake to withdraw");
+        uint256 balance = usersBalance[msg.sender];
+        if (balance == 0) revert NoActiveStake();
 
-        uint256 amountToWithdraw = usersBalance[msg.sender];
         usersBalance[msg.sender] = 0;
 
-        bool success = IERC20(stakingToken).transfer(msg.sender, amountToWithdraw);
-        require(success, "Failed to transfer staking token");
-        emit Withdraw(msg.sender, amountToWithdraw);
+        bool success = IERC20(stakingToken).transfer(msg.sender, balance);
+        if (!success) revert TransferFailed();
+        
+        emit Withdraw(msg.sender, balance);
     }
 
     function claimRewards() external { 
-        require(usersBalance[msg.sender] > 0, "You don't have an active stake to claim rewards from");
+        if (usersBalance[msg.sender] == 0) revert NoActiveStake();
 
         uint256 elapsePeriod = block.timestamp - depositTime[msg.sender];
-        require(elapsePeriod >= stakingPeriod, "Staking period has not yet elapsed. Need to wait.");
+        if (elapsePeriod < stakingPeriod) revert StakingPeriodNotElapsed();
 
         uint256 periods = elapsePeriod / stakingPeriod; 
         uint256 totalReward = periods * rewardPerPeriod;
@@ -72,12 +86,12 @@ contract StakingApp is Ownable {
         depositTime[msg.sender] += periods * stakingPeriod;
 
         (bool success, ) = msg.sender.call{value: totalReward}("");
-        require(success, "Failed to send reward");
+        if (!success) revert RewardTransferFailed();
 
         emit ClaimRewards(msg.sender, totalReward);
     }
 
-    receive() external payable onlyOwner {
+    receive() external payable {
         emit EtherSend(msg.value);
     }
 
